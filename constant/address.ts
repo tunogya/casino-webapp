6c6e8d16c0b0e27841840b34033e1046b750111f
tunogya <tunogya@qq.com>
2022-10-23 19:56:31 +0800

    update contract and abi

--- constant/address.ts
+++ constant/address.ts
@@ -1,11 +1,11 @@
 import {chain} from 'wagmi';
 
 export const SNATCH_ADDRESS = {
-  [chain.goerli.id]: "0x6040181fdf1281d9dAb495D2A576eC62c248a68F",
+  [chain.goerli.id]: "0x0286FD18bC8a3BcEB73722809eA548E3A887c00E",
 }
 
 export const FOUR_DUCKS_ADDRESS = {
-  [chain.goerli.id]: "0xcB12ee888B4Fd3e7ff29612087F093bD9Ee4f606"
+  [chain.goerli.id]: "0x698bdaadC1b2F4a687F690253d7f68Abfb5A862B"
 }
 
 export const NATIVE_CURRENCY_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"

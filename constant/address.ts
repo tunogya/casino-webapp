887fe561e7f0480ee9a658877b34b75ac7fdfda1
tunogya <tunogya@qq.com>
2022-10-28 10:29:41 +0800

    update

--- constant/address.ts
+++ constant/address.ts
@@ -1,11 +1,11 @@
 import {chain} from 'wagmi';
 
 export const SNATCH_ADDRESS = {
-  [chain.goerli.id]: "0x39e55b5E450b4e18d993B446C83086423e2E93F0",
+  [chain.goerli.id]: "0x0bA24032A01120d1bE9C0bB0fdBEd68e8C289E8A",
 }
 
 export const FOUR_DUCKS_ADDRESS = {
-  [chain.goerli.id]: "0x100a14Fd9F79EcC5AEFcDbbec1e6Fd0FA2a48A02"
+  [chain.goerli.id]: "0x97306f1c9679f5DE1c5223F2b2AC8EFBc5BF6caC"
 }
 
 export const NATIVE_CURRENCY_ADDRESS = "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"

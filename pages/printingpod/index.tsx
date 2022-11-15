b8a2a4ca93debf41eaad8331a9771140ca6a7791
tunogya <tunogya@qq.com>
2022-11-15 13:38:25 +0800

    add printingpod

--- pages/printingpod/index.tsx
+++ pages/printingpod/index.tsx
@@ -1,9 +1,23 @@
 import Layout from "../../components/layout";
+import {Button, Heading, HStack, Stack} from "@chakra-ui/react";
+import PrintingPodCard from "../../components/PrintingPodCard";
 
 const Printingpod = () => {
   return (
     <Layout>
-      <h1>Printingpod</h1>
+      <Stack h={'full'} w={'full'} align={"center"} p={['12px', '24px']} spacing={'48px'}>
+        <Heading>Printing Pod</Heading>
+        <Stack w={'full'} h={'full'} maxW={'container.md'} spacing={'48px'}>
+          <HStack spacing={'24px'} justify={"center"}>
+            <Button>Draw x3</Button>
+          </HStack>
+          <HStack spacing={'24px'} justify={"center"}>
+            <PrintingPodCard />
+            <PrintingPodCard />
+            <PrintingPodCard />
+          </HStack>
+        </Stack>
+      </Stack>
     </Layout>
   )
 }

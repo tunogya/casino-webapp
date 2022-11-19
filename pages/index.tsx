7dc24f2d5cd9a2b0f6e3112022790fde60f0e62f
tunogya <tunogya@qq.com>
2022-11-19 16:05:14 +0800

    add png

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,8 @@ const Home: NextPage = () => {
   return (
     <Layout>
       <Stack spacing={'11px'} w={'full'} h={'full'} p={'22px'} pb={'44px'}>
-        <Stack w={'full'} h={ Math.min(width || 640, 640) - 44 } bg={"#FEFAC0"} border={'2px'} borderRadius={'11px'} borderColor={'yellow.900'}>
+        <Stack w={'full'} h={ Math.min(width || 640, 640) - 44 } border={'2px'} bgImage={'/4ducks.png'} bgSize={'contain'} bgRepeat={'no-repeat'} bgPosition={'center'}
+               borderRadius={'11px'} borderColor={'yellow.900'}>
         </Stack>
         <Button fontSize={'14px'} minH={'75px'} bg={'#EB5370'} onClick={() => {
           router.push(`/fourducks/?id=${address || AddressZero}`)
@@ -23,7 +24,7 @@ const Home: NextPage = () => {
         </Button>
       </Stack>
       <Stack p={'44px 22px'} w={'full'} spacing={'22px'} h={'full'} bg={'#81D8C5'} align={"center"} borderTop={'2px'} borderColor={'yellow.900'}>
-        <Heading fontSize={'36px'}>Snatch Pool</Heading>
+        <Heading fontSize={'36px'} textAlign={"center"}>Snatch Pool</Heading>
         <Stack align={"center"} spacing={0}>
           <Text fontSize={'12px'}>The more draws, the higher the lucky value!</Text>
           <Text fontSize={'12px'} fontWeight={'bold'}>RP = 200, Special bonus!</Text>

f7ec64b75a9cf28324f68857b1ba00a86b18991a
tunogya <tunogya@qq.com>
2022-12-23 15:10:09 +0800

    update

--- pages/api/[address].ts
+++ pages/api/[address].ts
@@ -4,6 +4,10 @@ const {DynamoDBClient} = require("@aws-sdk/client-dynamodb");
 
 const ddbClient = new DynamoDBClient({
   region: 'ap-northeast-1',
+  credentials: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+  }
 });
 
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);

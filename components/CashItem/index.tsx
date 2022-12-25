2126e67b79e7f81134e3100290936accd68d2f57
tunogya <tunogya@qq.com>
2022-12-25 22:31:58 +0800

    update

--- components/CashItem/index.tsx
+++ components/CashItem/index.tsx
@@ -1,25 +1,33 @@
 import {Button} from "@chakra-ui/react";
 import {FC} from "react";
-import {useAccount, useNetwork, useToken} from "wagmi";
+import {Address, useAccount, useContractRead, useNetwork, useToken} from "wagmi";
+import {CASH_ADDRESS} from "../../constant/address";
+import CASH_ABI from "../../abis/CASH.json";
+import {BigNumber, ethers} from "ethers";
 
 type CashItemProps = {
   onClick: () => void;
-  token: string;
+  token:  Address;
 }
 
 const CashItem: FC<CashItemProps> = ({onClick, token }) => {
   const { address } = useAccount()
   const { chain } = useNetwork()
-  const { data, isLoading } = useToken({
-    // @ts-ignore
+  const { data: tokenData, isLoading: tokenIsLoading } = useToken({
     address: token,
     chainId: chain?.id,
     cacheTime: 30_000,
   })
+  const {data: cashBalance, isLoading: cashIsLoading} = useContractRead({
+    address: CASH_ADDRESS[chain?.id || 5],
+    abi: CASH_ABI,
+    functionName: "balanceOf",
+    args: [token, address],
+  })
 
   return (
-    <Button onClick={onClick} variant={'outline'} w={'full'} isLoading={isLoading} loadingText={'...'}>
-      {data?.symbol}
+    <Button onClick={onClick} variant={'outline'} w={'full'} isLoading={tokenIsLoading || cashIsLoading} loadingText={'...'}>
+      {ethers.utils.formatUnits(BigNumber.from(cashBalance || 0), tokenData?.decimals)} {tokenData?.symbol}
     </Button>
   )
 }
--- constant/address.ts
+++ constant/address.ts
@@ -1,13 +1,5 @@
 import {Address, chain} from 'wagmi';
 
-export const SNATCH_ADDRESS: {[p: number]: Address} = {
-  [chain.goerli.id]: "0x0bA24032A01120d1bE9C0bB0fdBEd68e8C289E8A",
-}
-
-export const FOUR_DUCKS_ADDRESS: {[p: number]: Address} = {
-  [chain.goerli.id]: "0x97306f1c9679f5DE1c5223F2b2AC8EFBc5BF6caC"
-}
-
-export const PRINTING_POD_ADDRESS: {[p: number]: Address} = {
-  [chain.goerli.id]: "0xB2eC8E843c27946Dd4789609E5a333917E0Ba215"
+export const CASH_ADDRESS: {[p: number]: Address} = {
+  [chain.goerli.id]: "0x3eF7c6f91f40A57203382F524F65E86bBAbFcEf5",
 }
